8e7eb3a74732e4b2c855e1e67489befaf5e035ab
Pingvu <nhlpool27@gmail.com>
2023-11-30 12:26:37 +0100

    Add project files.

---
+++ Uppgift4_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace uppgift4__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 31; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int x = 200; x < 180; x--)
+            {
+                Console.Write(x);
+            }
+            for (int y = 1000; y < 1401; y += 50)
+            {
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
